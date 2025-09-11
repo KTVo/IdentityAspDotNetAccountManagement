95d210012711e8f2cdafd35751d029e10e6c2555
Kevin Vo <KTVo@users.noreply.github.com>
2025-09-11 07:39:59 -0700

    Added user registration portal for ASP.Net Identity.
    Added user registration portal for ASP.Net Identity.

--- DotNet9EFAPI/DotNet9EFAPI/MVCS/Services/_DB/TestDBContext.cs
+++ DotNet9EFAPI/DotNet9EFAPI/MVCS/Services/_DB/TestDBContext.cs
@@ -1,4 +1,5 @@
 using DotNet9EFAPI.MVCS.Models.DB;
+using DotNet9EFAPI.MVCS.Models.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace DotNet9EFAPI.MVCS.Services._DB;
@@ -7,8 +8,9 @@ public class TestDBContext : DbContext
 {
 
     public TestDBContext(DbContextOptions<TestDBContext> options) : base(options) { }
-   
+
     // INSTANTIATE C# MODELS FOR THE DATABASE(S)
     public DbSet<Item>? Items { get; set; }
+    public DbSet<User>? Users { get; set; }
 
 }
--- DotNet9EFAPI/DotNet9EFAPI/Program.cs
+++ DotNet9EFAPI/DotNet9EFAPI/Program.cs
@@ -1,8 +1,14 @@
+using DotNet9EFAPI.MVCS.Models.Identity;
 using DotNet9EFAPI.MVCS.Services._DB;
 using DotNet9EFAPI.MVCS.Services.Test;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthorization();
+builder.Services.AddIdentityApiEndpoints<User>()
+    .AddEntityFrameworkStores<TestDBContext>();
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -13,7 +19,23 @@ builder.Services.AddControllers();
 builder.Services.AddScoped<ITestService, TestService>();
 
 builder.Services.AddDbContext<TestDBContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services
+    .AddIdentityCore<User>(options =>
+    {
+        options.User.RequireUniqueEmail = true;
+        // configure password/lockout/etc here if you want
+    })
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<TestDBContext>()
+    .AddSignInManager()
+    .AddDefaultTokenProviders();
+
+
+
 var app = builder.Build();
+
+app.MapIdentityApi<User>();
 
 // Middleware
 app.UseRouting();
@@ -29,28 +51,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+
